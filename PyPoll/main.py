4d6f94ccc458c946479c47a7cb2ce96482c128d2
Michael Cherkassky <MJCherkassky@gmail.com>
2020-04-01 10:09:12 -0500

    Final Tweaks

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,17 +1,10 @@
 # The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create a Python script that analyzes the votes and calculates each of the following:
 
-
-
 #The percentage of votes each candidate won
-
-
-
 #The winner of the election based on popular vote.
-
 import os
 import csv
 votes = {} #Set up a dictionary for candidates as key and vote count as value
-
 csvpath = os.path.join("..", "..", "..", "Classwork", "vu-nsh-data-pt-02-2020-u-c", "03-Python", "Homework", "Instructions", "PyPoll", "Resources", "election_data.csv")
 with open(csvpath) as csvfile:
     csvreader=csv.reader(csvfile, delimiter=",")
@@ -31,28 +24,19 @@ Total Votes: {total_votes}\n~~~~~~~~~~~~~~~
     statement2 = f"""
 ~~~~~~~~~~~~~~~\nWinner: {winner} \n~~~~~~~~~~~~~~~
     """
-    
+    print(statement1)    
     if not os.path.isdir("Output"):
         os.mkdir("Output")
     else:
-        pass
-    
+        pass  
     output_path = os.path.join("Output", "ElectionResults.txt")
     with open(output_path, "w+") as writedata:
         writedata.writelines(statement1)
         for i in votes:
             popularvote = (votes[i]/total_votes)
+            print(f"{i} : {popularvote:.3%} ({votes[i]})")
             writedata.writelines(f"""
-{i}: {popularvote:.3%} ({votes[i]})\n""") #The total number of votes for each candidate 
+{i}: {popularvote:.3%} ({votes[i]})""") #The total number of votes for each candidate 
         winner = max(votes, key=votes.get)
         writedata.writelines(statement2)
-
-
-
-    # outputfile = "output.txt"
-    # with open(outputfile, "w") as doc:
-    #     # doc.write(csvfile)
-   
-    
-
-        
+print(statement2)
